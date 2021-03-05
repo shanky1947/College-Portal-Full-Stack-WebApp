0e7b96b0f04b0ddb65a742bdd76c771c588d36ba
shashank <shashank.shukla1947@gmail.com>
2021-03-05 20:13:21 +0530

    added plagiarism part

---
+++ Python files/data_func.py
@@ -0,0 +1,47 @@
+from io import StringIO
+
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
+
+def convert_pdf_to_string(file_path):
+
+	output_string = StringIO()
+	with open(file_path, 'rb') as in_file:
+	    parser = PDFParser(in_file)
+	    doc = PDFDocument(parser)
+	    rsrcmgr = PDFResourceManager()
+	    device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+	    interpreter = PDFPageInterpreter(rsrcmgr, device)
+	    for page in PDFPage.create_pages(doc):
+	        interpreter.process_page(page)
+
+	return(output_string.getvalue())
+
+                
+def convert_title_to_filename(title):
+    filename = title.lower()
+    filename = filename.replace(' ', '_')
+    return filename
+
+
+def split_to_title_and_pagenum(table_of_contents_entry):
+    title_and_pagenum = table_of_contents_entry.strip()
+    
+    title = None
+    pagenum = None
+    
+    if len(title_and_pagenum) > 0:
+        if title_and_pagenum[-1].isdigit():
+            i = -2
+            while title_and_pagenum[i].isdigit():
+                i -= 1
+
+            title = title_and_pagenum[:i].strip()
+            pagenum = int(title_and_pagenum[i:].strip())
+        
+    return title, pagenum
+    
